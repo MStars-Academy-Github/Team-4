d2a54fef4c066b190c324166eccf22167ed3fa96
kurtzkaranq <mendbayarerxes@gmail.com>
2022-09-05 17:35:36 +0800

    checking

--- Project-2/front-end/components/mainHeader.tsx
+++ Project-2/front-end/components/mainHeader.tsx
@@ -9,7 +9,6 @@ import {
 import { MdOutlineNotificationsActive } from "react-icons/md";
 import { RiUserReceivedLine } from "react-icons/ri";
 import LeftSide from "./LeftSide";
-import Toggle from "./Toggle";
 import Player from "./Player";
 import dynamic from "next/dynamic";
 import { IVideos } from "../types/types";
@@ -18,8 +17,25 @@ export default function MainHeader() {
   const router = useRouter();
   const [isOpen, setIsopen] = useState(false);
   const [videos, setVideos] = useState<IVideos[] | undefined>([]);
-  const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
+  const [search, setSearch] = useState<IVideos[] | []>([]);
+  const [checker, setChecker] = useState<boolean>(true);
 
+  function searchHandler(e: any) {
+    e.preventDefault();
+    videos?.forEach((p: IVideos, i: number) => {
+      if (p.title.includes(e.target[0].value)) {
+        setSearch([]);
+        setSearch([...search, p]);
+      }
+    });
+    if (search.length <= 0) {
+      console.log("no such video");
+    } else {
+      console.log("video exists");
+    }
+    console.log(search);
+    setChecker(false);
+  }
   const ToggleSidebar = () => {
     isOpen === true ? setIsopen(false) : setIsopen(true);
   };
@@ -51,7 +67,13 @@ export default function MainHeader() {
             />
           </a>
         </div>
-        <form action="" className="searchForm flex ">
+        <form
+          action=""
+          className="searchForm flex "
+          onSubmit={(e: any) => {
+            searchHandler(e);
+          }}
+        >
           <input
             type="search"
             className=""
@@ -81,7 +103,7 @@ export default function MainHeader() {
       </div>
 
       <div className="flex  w-full justify-between">
-        {isOpen == true ? <Toggle /> : <LeftSide />}
+        {/* {isOpen == true ? <Toggle /> : <LeftSide />} */}
         <div className="flex flex-wrap">
           <div className="headerButtons flex justify-around w-full h-[50px] bg-[#fff] border-t-black">
             <button className="hover:bg-slate-100">All</button>
@@ -91,9 +113,18 @@ export default function MainHeader() {
             <button>Nature</button>
             <button>Cartoon</button>
           </div>
-          {videos?.map((e: IVideos, i: number) => {
-            return <Player e={e} i={i} />;
-          })}
+          {checker
+            ? videos?.map((e: IVideos, i: number) => {
+                return <Player e={e} i={i} />;
+              })
+            : search?.map((e: IVideos, i: number) => {
+                return (
+                  <div>
+                    <p>searched videos</p>
+                    <Player e={e} i={i} />
+                  </div>
+                );
+              })}
         </div>
       </div>
     </div>
