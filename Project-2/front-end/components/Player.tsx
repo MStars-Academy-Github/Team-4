bf7055a0980afb555dc990ce71e3e80548fa030e
kurtzkaranq <mendbayarerxes@gmail.com>
2022-09-05 16:55:20 +0800

    fixed

--- Project-2/front-end/components/Player.tsx
+++ Project-2/front-end/components/Player.tsx
@@ -4,15 +4,17 @@ import React, { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
 import { list } from "../pages/api/api.media";
 import { IVideos, PlayerProps } from "../types/types";
-// import dynamic from "next/dynamic";
-
 export default function Player({ e, i }: PlayerProps) {
   const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
   return (
-    <div className="w-[20%] bg-slate-100 h-[250px]">
+    <div className="w-[450px] h-[250px]">
       <p>testing</p>
-      <ReactPlayer e={e} i={i} />
+      <ReactPlayer
+        controls
+        key={i}
+        url={`http://localhost:4000/v1/media/video/${e._id}`}
+      />
     </div>
   );
 }
